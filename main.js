cab266b77fdbc0e69865f77f9abb0fc9666cdf7f
shahzafrani <szz786@gmail.com>
2016-11-06 01:11:54 -0500

    added left keyboard input

--- main.js
+++ main.js
@@ -7,6 +7,11 @@ var resources = PIXI.loader.resources;
 var Sprite = PIXI.Sprite;
 //Add the canvas to the HTML document
 document.body.appendChild(renderer.view);
+//Capture the keyboard arrow keys
+var left = keyboard(37),
+    up = keyboard(38),
+    right = keyboard(39),
+    down = keyboard(40);
 
 //Create a container object called the `stage`
 var stage = new PIXI.Container();
@@ -18,7 +23,7 @@ renderer.render(stage);
 loader
   .add('images/ship4.gif')
   .load(setup);
-var state
+var state;
 
 
 function setup(){
@@ -26,4 +31,41 @@ function setup(){
 ship = new Sprite(resources['images/ship4.gif'].texture);
 stage.addChild(ship);
 renderer.render(stage);
+ship.vx = 0;
+ship.vy = 0;
+state = play;
+gameLoop();
+//Left arrow key `press` method
+left.press = function() {
+
+  //Change the ship's velocity when the key is pressed
+  ship.vx = -5;
+  ship.vy = 0;
+};
+
+//Left arrow key `release` method
+left.release = function() {
+
+  //If the left arrow has been released, and the right arrow isn't down,
+  //and the ship isn't moving vertically:
+  //Stop the ship
+  if (!right.isDown && ship.vy === 0) {
+    ship.vx = 0;
+  }
+};
+}
+
+console.log(setup);
+function gameLoop(){
+  requestAnimationFrame(gameLoop);
+
+  state();
+
+  renderer.render(stage);
+}
+function play(){
+  //Capture the keyboard arrow keys
+  ship.x += ship.vx;
+    ship.y += ship.vy;
+
 }
